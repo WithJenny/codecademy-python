5b1cb038f5a98260a520c15d0b26ba48e832a6df
Jenny Tran <withjennyy@gmail.com>
2022-06-18 15:55:42 -0700

    create martini with print statements

---
+++ chapter1/block-letters.py
@@ -0,0 +1,24 @@
+ # IG :jennyytrann_
+# Fun Fact: I will buy an American Eskimo once I switch into tech
+
+print("                              O       ")
+print("                             O        ")
+print("                            O	     ")	
+print("            CCCCCCCCCCCCCCCCCC        ")
+print("             CCCCCCCCCCCCCCCC         ") 
+print("              CCCCCCCCCCCCCC          ")
+print("               CCCCCCCCCCCC           ")
+print("                CCCCCCCCCC            ")
+print("                 CCCCCCCC             ")
+print("                  CCCCCC              ")
+print("                   CCCC               ")  
+print("                    CC                ") 
+print("                    CC                ")
+print("                    CC                ")  
+print("                    CC                ")
+print("                    CC                ")
+print("                    CC                ")
+print("                    CC                ")
+print("                    CC                ")
+print("                CCCCCCCCCC            ")
+
